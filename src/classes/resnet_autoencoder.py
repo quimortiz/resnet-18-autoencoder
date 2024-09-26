582f631b24ac5af50b468e83fb5ee86d24bf97ca
quim@mango <quimortiz21@gmail.com>
2024-09-26 16:20:48 -0400

    add a bottleneck for doing motion planning

--- src/classes/resnet_autoencoder.py
+++ src/classes/resnet_autoencoder.py
@@ -55,6 +55,68 @@ class AE(nn.Module):
         else:
                 raise NotImplementedError("Only default and light resnet have been implemented. Th light version corresponds to input datasets with size less than 64x64.")
 
+       # Convolution layer to reduce channels from 64 to 32
+        self.enc_conv = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
+        
+        # Pooling layer to reduce spatial dimensions from 16x16 to 8x8
+        self.enc_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Fully connected layer to reduce from 32 * 8 * 8 = 2048 to 8 dimensions
+        self.enc_fc = nn.Linear(32 * 8 * 8, 8)
+
+
+
+       # Fully connected layer to expand 8-dim vector to 32 * 8 * 8
+        self.dec_fc = nn.Linear(8, 32 * 8 * 8)
+        
+        # self.deconv = nn.ConvTranspose2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1)
+
+
+        # Transposed conv layer to go from 32x8x8 to 64x16x16
+        self.dec_deconv1 = nn.ConvTranspose2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.dec_bn1 = nn.BatchNorm2d(64)
+        
+        # Transposed conv layer to go from 64x16x16 to 64x32x32
+        self.dec_deconv2 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.dec_bn2 = nn.BatchNorm2d(64)
+
+
+
+
+    def encode(self,x):
+        """
+        """
+        z = self.encoder(x)
+
+        # print("original z" , z.shape)
+        #
+        z = self.enc_conv(z)
+        z = self.enc_pool(z)
+        z = z.view(-1, 32 * 8 * 8)
+        z = self.enc_fc(z)
+        return z, None , None
+
+    def decode(self,z):
+        """
+        """
+
+        y = self.dec_fc(z)
+        y = y.view(y.size(0), 32, 8, 8)  # Reshape to (batch_size, 32, 8, 8)
+
+        # First transposed convolution: (batch_size, 32, 8, 8) -> (batch_size, 64, 16, 16)
+        y = self.dec_deconv1(y)
+        y = self.dec_bn1(y)
+        y = nn.ReLU()(y)
+
+        # Second transposed convolution: (batch_size, 64, 16, 16) -> (batch_size, 64, 32, 32)
+        # y = self.dec_deconv2(y)
+        # y = self.dec_bn2(y)
+        # y = nn.ReLU()(y)
+        # print("yshape" , y.shape)
+        y = self.decoder(y)
+        return y
+
+
     def forward(self, x):
         """The forward functon of the model.
 
@@ -65,7 +127,10 @@ class AE(nn.Module):
             x (torch.tensor): encoder result
             z (torch.tensor): decoder result
         """
-        z = self.encoder(x)
-        x = self.decoder(z)
-        return x, z
+        z, _, _ = self.encode(x)
+
+        y = self.decode(z)
+
+
+        return y, z
     
